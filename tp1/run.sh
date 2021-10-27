439f12828a3ddb6b55d31a5af5bc6dd30f32851e
Pintademijote <pg.lemasle@gmail.com>
2021-10-27 15:48:25 +0200

    add condition

--- tp1/run.sh
+++ tp1/run.sh
@@ -2,5 +2,7 @@
 export ANSIBLE_CONFIG=/home/vagrant/training_ansible/tp1/.ansible.cfg
 ansible client1 -m ping 
 ansible client1 -m file -a 'path=toto.txt state=touch'
+if [ ! grep -q "client2" /home/vagrant/training_ansible/tp1/hosts ]; then
 echo "client2 ansible_host=192.168.99.12 ansible_user=vagrant ansible_password=vagrant ansible_ssh_common_args='-o StrictHostKeyChecking=no'" >> /home/vagrant/training_ansible/tp1/hosts
+fi
 ansible all -m ping 

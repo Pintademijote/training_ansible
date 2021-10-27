01d8aa8dcb9da0c7e7b64a86a0c94f7688ecfcfe
Pintademijote <pg.lemasle@gmail.com>
2021-10-27 15:43:27 +0200

    fix single quote to double in .sh

--- tp1/run.sh
+++ tp1/run.sh
@@ -2,5 +2,5 @@
 export ANSIBLE_CONFIG=/home/vagrant/training_ansible/tp1/.ansible.cfg
 ansible client1 -m ping 
 ansible client1 -m file -a 'path=toto.txt state=touch'
-echo 'client2 ansible_host=192.168.99.12 ansible_user=vagrant ansible_password=vagrant ansible_ssh_common_args='-o StrictHostKeyChecking=no'' >> /home/vagrant/training_ansible/tp1/hosts
+echo "client2 ansible_host=192.168.99.12 ansible_user=vagrant ansible_password=vagrant ansible_ssh_common_args='-o StrictHostKeyChecking=no'" >> /home/vagrant/training_ansible/tp1/hosts
 ansible all -m ping 

48418860494dab4e43cfd2c2e950ac764eaea0e2
Pintademijote <pg.lemasle@gmail.com>
2021-10-27 15:20:43 +0200

    add quote

--- tp1/run.sh
+++ tp1/run.sh
@@ -1,3 +1,3 @@
 #ansible [pattern] -m [ansible.builtin.ping] -a "[module options]"
-export $ANSIBLE_CONFIG=/home/vagrant/training_ansible/tp1/.ansible.cfg
+export $ANSIBLE_CONFIG='/home/vagrant/training_ansible/tp1/.ansible.cfg'
 ansible all -a ping 

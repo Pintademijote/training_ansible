54a4e04f68471c084a90abb3f4a15c013f4624f6
Pintademijote <pg.lemasle@gmail.com>
2021-10-28 11:48:24 +0200

    add command to run.sh & fix yml variable

--- tp3/run.sh
+++ tp3/run.sh
@@ -1,5 +1,7 @@
 #ansible [pattern] -m [ansible.builtin.ping] -a "[module options]"
 export ANSIBLE_CONFIG=/home/vagrant/training_ansible/tp3/.ansible.cfg
-ansible prod -m ping 
-ansible prod -m file -a 'path=toto.txt state=touch'
-ansible prod -m setup -a 'filter=ansible_memtotal_mb' 
+ansible prod -m ping -i hosts.ini
+ansible ansible -m setup -a 'filter=*name*'
+ansible-inventory prod --graph
+ansible-inventory all --graph
+ansible-inventory --host client1
